67810ca869b0cec564609c10397f601ff5e95124
Ryan Tablada <ryan.tablada@gmail.com>
2017-02-21 12:22:27 -0600

    Redux example

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
 import 'whatwg-fetch';
+import { createStore } from 'redux';
+
+const counter = createStore((state, action) => {
+  if (action.type === 'ADD') {
+    return state + 1;
+  }
+
+  return state;
+}, 0, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+counter.subscribe(() => {
+  document.querySelector('.count').innerText = counter.getState();
+});
+
+document.querySelector('.button').addEventListener('click', () => {
+  counter.dispatch({ type: 'ADD' });
+});
+
+counter.dispatch({ type: 'RESET' });
